e930464bc429f752aa14c541039b710ed22261fd
Rowan A <rowanaustin1@gmail.com>
2023-04-10 23:30:49 +0100

    DisplayData

---
+++ Data/DisplayData.cs
@@ -0,0 +1,14 @@
+﻿namespace RadioHeardleServer.Data
+{
+	public struct DisplayData
+	{
+		public SongData _songData;
+		public DateTime _updateDate;
+
+		public DisplayData(SongData songData, DateTime updated)
+		{
+			_songData = songData;
+			_updateDate = updated;
+		}
+	}
+}
