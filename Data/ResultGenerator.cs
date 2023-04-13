4db617bc44df2fa3060f6a5c7fdae3a82ead9a81
Rowan A <rowanaustin1@gmail.com>
2023-04-13 17:37:10 +0100

    re-add missed files in Data

---
+++ Data/ResultGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace RadioHeardleServer.Data
+{
+	public static class ResultGenerator
+	{
+		private static readonly string whitebox = "2B1C";
+		private static readonly string yellowbox = "1F7E8";
+		private static readonly string greenbox = "1F7E9";
+
+		public static string GetResultText(int guessesAllowed, string guessedOrSkipped, string edition)
+		{
+			StringBuilder unicodeBuilder = new StringBuilder("Radioheardle: " + edition + "\n");
+
+			if (guessedOrSkipped.Length > 0)
+			{
+				for (int i = 0; i < guessedOrSkipped.Length; i++)
+				{
+					switch (guessedOrSkipped[i])
+					{
+						case 'g':
+							unicodeBuilder.Append(char.ConvertFromUtf32(Convert.ToInt32(yellowbox, 16)));
+							break;
+						case 's':
+							unicodeBuilder.Append(char.ConvertFromUtf32(Convert.ToInt32(whitebox, 16)));
+							break;
+						default:
+							unicodeBuilder.Append(char.ConvertFromUtf32(Convert.ToInt32(greenbox, 16)));
+							break;
+					}
+				}
+			}
+
+			unicodeBuilder.Append("\n");
+			unicodeBuilder.Append("http://radioheardle.homieapathy.com");
+
+			return unicodeBuilder.ToString();
+		}
+	}
+
+}
---
+++ Data/SongData.cs
@@ -0,0 +1,25 @@
+﻿namespace RadioHeardleServer.Data
+{
+	public struct SongData
+	{
+		public string _fileName;
+		public string _songName;
+
+		public SongData(string fileName, string songName)
+		{
+			_fileName = fileName;
+			_songName = songName;
+		}
+
+		public SongData(string[] data)
+		{
+			_fileName = data[0];
+			_songName = data[1];
+		}
+
+		public override string ToString()
+		{
+			return _fileName + "\n" + _songName;
+		}
+	}
+}
